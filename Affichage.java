d79a02bbdda535bc1a4a5898c08f890c4afa6883
ODufossez <oscardufossez@gmail.com>
2025-03-13 09:03:45 +0100

    Terminé la mise en forme
    Changement de nom du texte d'intro
    Correction dans les doc concernés

--- Affichage.java
+++ Affichage.java
@@ -1,13 +1,15 @@
 import java.awt.Font;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Affichage {
-    public static void affichage() throws IOException {
+    public static void affichage(String path) throws IOException {
 
-        Font font = new Font("Arial" , Font.BOLD , 20);
+        Font font = new Font("Arial", Font.BOLD , 15);
         StdDraw.setXscale(-1920,1920);
         StdDraw.setYscale(-1080,1080);
         StdDraw.setCanvasSize(1920,1080); //full screen
@@ -15,7 +17,17 @@ public class Affichage {
         StdDraw.setTitle("Citadelle du Chaos");
         StdDraw.setFont(font);
 
-        //TODO trouver un moyen de revenir à la ligne
-        StdDraw.text(-1900,1000, Files.readString(Path.of("CitadelleDuChaos/ressources/text.txt")));
+        try(FileReader fileReader = new FileReader(path);
+            BufferedReader buff = new BufferedReader(fileReader)){
+            String line;
+            int indiceY = 1060;
+
+            while ((line = buff.readLine()) != null) {
+                StdDraw.text(-150,indiceY,line);
+                indiceY=indiceY-40;
+            }
+        } catch (IOException e) {
+            System.exit(2);
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -3,6 +3,5 @@ import java.io.IOException;
 //non défintif
 public class Main {
     public static void main(String[] args) throws IOException {
-        Affichage.affichage();
     }
 }
