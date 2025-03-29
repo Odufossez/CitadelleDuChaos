a9d4a902570235fa8ee349a14e2d28193f5249ab
ODufossez <oscardufossez@gmail.com>
2025-03-29 13:20:07 +0100

    wip MainMenu

--- src/Visual/MainMenu.java
+++ src/Visual/MainMenu.java
@@ -1,13 +1,16 @@
 package Visual;
 
+import Methods.Events;
 import Tools.StdDraw;
 import java.io.File;
+import java.io.IOException;
+
 import static  Tools.Fonts.*;
 import static Tools.PathsTo.*;
 
 public class MainMenu {
 
-    public void afficherTextes(){
+    public void afficherTextes() throws IOException {
         int x = -1800;
 
         /*Titre principal*/
@@ -20,18 +23,65 @@ public class MainMenu {
         StdDraw.setFont(plain32);
 
         StdDraw.textLeft(x , 80 , "1 - Nouvelle partie");
-        
+
         if (!savesExist()) {
-            StdDraw.setPenColor(StdDraw.DARK_GRAY);
+            StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
         }
         StdDraw.textLeft(x,0,"2 - Charger une sauvegarde");
         StdDraw.setPenColor(StdDraw.BLACK);
         StdDraw.textLeft(x,-80 , "3 - Crédits (wip)");
-        StdDraw.textLeft(x,-160,"4 - Sortir");
+        StdDraw.textLeft(x,-160,"4 - Succès (wip)");
+        StdDraw.textLeft(x,-240,"5 - Sortir");
 
         StdDraw.show();
+        char inChar;
+
+        do {
+            while (!StdDraw.hasNextKeyTyped()){continue;}
+            inChar = StdDraw.nextKeyTyped();
+            switch (inChar){
+                case '1' , '&' : {
+                    if (savesExist()){
+                        StdDraw.clear();
+                        Affichage.choiceSaveLoad();
+                    } else {
+                        StdDraw.clear();
+                        CharacterCreator.creationPerso(1);
+                    }
+                    break;
+                }
+                case '2' , 'é' : {
+                    if (savesExist()){
+                        StdDraw.clear();
+                        Affichage.choiceSaveLoad();
+                    }
+                    break;
+                }
+                case '3' , '"' : {
+
+                    break;
+                }
+                case '4' , '\'' : {
+
+                    break;
+                }
+                case '5' , '(' : {
+
+                    break;
+                }
+                default: break;
+            }
+        }while(inChar!='1' && inChar!='&' &&
+                (inChar!='2'&&savesExist()) && (inChar!='é'&&savesExist()) &&
+                inChar!='"' && inChar!='3' &&
+                inChar!='\'' && inChar!='4' &&
+                inChar!='5' && inChar!='(');
     }
 
+    /**
+     * Permet de savoir s'il existe déjà une sauvegarde
+     * @return true s'il y a au moins une sauvegarde sinon false
+     */
     public static boolean savesExist(){
         for (int i = 1; i < 4 ; i++) {
             File file = new File(savePath + i + ".txt");
