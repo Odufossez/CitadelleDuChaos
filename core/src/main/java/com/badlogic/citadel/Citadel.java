69fb739d135585243cebd646ceceb31c089dee94
ODufossez <oscardufossez@gmail.com>
2025-10-20 21:22:30 +0200

    Update Citadel.java
    nettoyage suite reformats

--- core/src/main/java/com/badlogic/citadel/Citadel.java
+++ core/src/main/java/com/badlogic/citadel/Citadel.java
@@ -11,35 +11,11 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Citadel extends Game {
-    public final static int MAINMENU = 0;
-    public final static int LOADMENU = 1;
-    public final static int CHARACTERCREATOR = 2;
-    public final static int SPELLSMENU = 3;
-    public final static int INVENTORY = 4;
-    public final static int GAMEOVER = 5;
-    public final static int PARAMETERS = 6;
-    public final static int SUMMARYSCREEN = 7;
-    public final static int RULESSCREEN = 8; //TODO
-
-    public final static int INTROSCREEN = 10;
-
-    //public BitmapFont titleFont = new BitmapFont(); //todo revoir les fonts
-    public static BitmapFont versionFont;
 
     public SpriteBatch batch;
 
     private MainMenuScreen menuScreen;
-    private SpellScreen spellScreen;
-    private GameOverScreen gameOverScreen;
-    private CharacterCreatorScreen cCScreen;
-    private InventoryScreen inventoryScreen;
-    private LoadSaveScreen loadScreen;
-    private ParameterScreen parameterScreen;
-    private SummaryScreen summaryScreen;
 
-    //Events screen
-    private IntroGameScreen introScreen;
-    private AtTheGateGameScreen atTheGateGameScreen;
 
     private AppPreferences appPrefs;
     public static float density;
@@ -56,16 +32,24 @@ public class Citadel extends Game {
         density = Gdx.graphics.getDensity();
 
         //TODO à changer pour la version finale -- player et grimoire virtuel pour les tests
-        /*player = new Player(1,1,1,1,1);
-        player.setCurrentMagic(1);
-        player.setCurrentLuck(1);
-        player.setCurrentVitality(1);
-        SpellBook grim = new SpellBook(1);
+        player = new Player(10,15,9,0,10);
+        player.setCurrentMagic(9);
+        player.setCurrentLuck(10);
+        player.setCurrentVitality(15);
+        SpellBook grim = new SpellBook(9);
         grim.putIn(SpellList.Sorts.HABILITE);
-        introScreen = new IntroGameScreen(this);
-        setScreen(introScreen);*/
+        grim.putIn(SpellList.Sorts.CHANCE);
+        grim.putIn(SpellList.Sorts.ENDURANCE);
+        grim.putIn(SpellList.Sorts.LEVITATION);
+        grim.putIn(SpellList.Sorts.OR_DU_SOT);
+        grim.putIn(SpellList.Sorts.PROTECTION);
+        grim.putIn(SpellList.Sorts.TELEPATHIE);
+        grim.putIn(SpellList.Sorts.ILLUSION);
+        grim.putIn(SpellList.Sorts.FORCE);
+        IntroGameScreen introScreen = new IntroGameScreen(this);
+        setScreen(introScreen);
         /*------ fin du temporaire ---*/
-        setScreen(menuScreen);
+        //setScreen(menuScreen);
 
     }
 
